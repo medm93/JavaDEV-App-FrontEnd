b8dc75f856076ad7c1b795a34cbaf478d36b0a39
MEDM <m.e.misiak@gmail.com>
2019-02-20 18:07:02 +0100

    Created mock users

---
+++ src/app/mock-users.ts
@@ -0,0 +1,6 @@
+import { User } from './user';
+
+export const USERS: User[] = [
+	{id: 1, firstName: 'Clint', lastName: 'Barton', email: 'hawkeye@marvel.com', password: '{noop}zaq1@WSX', yearOfStudy: '1', fieldOfStudy: 'Automatics', indexNumber: '000001'},
+	{id: 2, firstName: 'Steven', lastName: 'Rogers', email: 'capitan.america@marvel.com', password: '{noop}zaq1@WSX', yearOfStudy: '2', fieldOfStudy: 'Electronics', indexNumber: '000002'}
+]
